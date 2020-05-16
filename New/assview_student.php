de499aa751b5b663c5ad5d78993bd66f9d61f103
Bidisha <officialbidisha1@gmail.com>
2020-05-16 12:57:59 +0530

    Student side partial

--- New/assview_student.php
+++ New/assview_student.php
@@ -1,17 +1,17 @@
 <?php
-        session_start();
-        include("../includes/db.php");
-        error_reporting(0);
-        if(isset($_SESSION['username'])){
-            $username=$_SESSION['username'];//has to be logged in
-            $semester=$_SESSION['sem'];
-         }
-        else
-                header( "refresh:2; url=login_student.php" );
-        
+session_start();
+include("../includes/db.php");
+error_reporting(0);
+if (isset($_SESSION['username'])) {
+  $username = $_SESSION['username']; //has to be logged in
+  $semester = $_SESSION['sem'];
+} else
+  header("refresh:2; url=login_student.php");
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <title>Bootstrap Example</title>
   <meta charset="utf-8">
@@ -20,81 +20,126 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  <script src="navigation.js"></script>
+  <link href="navigation.css" rel="stylesheet" id="naviagtion-css">
+  <link rel="stylesheet" href="cardstyles.css">
+  <link rel="stylesheet" href="tablestyle.css">
+  <link rel="stylesheet" href="formborder.css">
+  <style>
+    body {
+      background-color: #ededed;
+      background-repeat: no-repeat;
+      background-position: center;
+      background-size: contain;
+    }
+  </style>
 </head>
-<body>
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-  <ul class="navbar-nav">
-    <li class="nav-item active">
-      <a class="nav-link" href="#">Student Dashboard</a>
-    </li>
-  </ul>
-</nav>
-<div class="container mt-5">
-    <form action=" " method="post">
-        <h3>View Assignment</h3>
-            <!-----<form>------>
-                <div class="form-group">
-                        <label for="subject">Select Subject</label>
-                            <select class="form-control" id="subjectControlSelect1" name="subject">           
-                                <?php
-                                        $get_subject="select * from sem_subject where sem=$semester"; 	
-                                        $run_subject=mysqli_query($con,$get_subject);
-                                        while($row_subject=mysqli_fetch_array($run_subject))
-                                        {
-                                            $subject=$row_subject['subject'];
-                                            echo "<option>$subject</option>";
-                                        }
-                                                
-                                ?>            
-                            </select>
-                            <div class="p-1"></div>
-                            <button type="submit" name="view" class="btn btn-primary">View</button>
-                </div>
-    </form>
-    <?php
-              if(isset($_POST['view']) && isset($_POST['subject'])){
-                  
-          $subject=$_POST['subject'];
-          $query="SELECT * FROM assignment_new where subject='$subject'";
-					$data=mysqli_query($con,$query);
-					$total=mysqli_num_rows($data);
-                    if($total>0){
-    ?>                
-                        <table class="table">
-                        <thead>
-                                <tr>
-                                        <th scope="col">Serial No.</th>
-                                        <th scope="col">Date</th>
-                                        <th scope="col">Assignment</th>
-                                </tr>
-                        </thead>    
-                        <?php
-                                $counter=1;       
-                                while($result =mysqli_fetch_array($data))
-                                {                        
-				        ?>
-                                    <tbody>
-                                    <tr>
-                                        <th scope="row"><?php echo $counter++ ;?></th>
-                                        <td><?php echo $result['submit_date'] ?></td>
-                                        <td><a href="<?php echo $result['link'] ?>">Click Here</a></td>
-                                                                
-                                    </tr>
-                                    </tbody>
 
-				    <?php
-                                }
-                    }
-                     
-                    else
-                    {
-                            echo "No Assignment Uploaded Yet";
-                    }
-            }
-?>
-    
-</table>              
-    
-</div>
+<body>
+  <nav class="navbar navbar-expand navbar-dark bg-primary fixed-top"> <a href="#menu-toggle" id="menu-toggle" class="navbar-brand"><span class="navbar-toggler-icon"></span></a> <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+    <div class="collapse navbar-collapse" id="navbarsExample02">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active"> <a class="nav-link" href="firstteacher.php">Dashboard</a> </li>
+      </ul>
+      <form class="form-inline my-2 my-md-0"> </form>
+    </div>
+  </nav>
+  <div id="wrapper" class="toggled">
+    <div id="sidebar-wrapper">
+      <ul class="sidebar-nav">
+        <li class="sidebar-brand"> <a href="firstteacher.php"> <?php echo $_SESSION['username'] ?> </a> </li>
+        <li> <a href="firstteacher.php">Dashboard</a> </li>
+        <li> <a href="assignmentinside.php">Assignments</a> </li>
+        <li> <a href="noticeview.php">Notices</a> </li>
+        <li> <a href="studyupload.php">Study Material</a> </li>
+        <li> <a href="#">Contact</a> </li>
+      </ul>
+    </div> <!-- /#sidebar-wrapper -->
+    <div id="page-content-wrapper">
+      <div class="container mt-3 custom-form-container " style="width:1000px; margin:0 auto;">
+        <center>
+          <h2>View Assignment</h2>
+        </center>
+        <form action=" " method="post">
+
+          <!-----<form>------>
+          <div class="form-group">
+            <div class="row d-flex justify-content-center">
+              <div class="text-center ">
+                <label for="subject">Subject</label>
+              </div>
+              <div class="p-3"></div>
+              <div class=" form-group col-xs-6 w-25">
+
+                <select class="form-control" id="subjectControlSelect1" name="subject">
+                  <?php
+                  $get_subject = "select * from sem_subject where sem=$semester";
+                  $run_subject = mysqli_query($con, $get_subject);
+                  while ($row_subject = mysqli_fetch_array($run_subject)) {
+                    $subject = $row_subject['subject'];
+                    echo "<option>$subject</option>";
+                  }
+
+                  ?>
+                </select>
+              </div>
+              <div class="p-2"></div>
+              <div class="text-center col-xs-6 ">
+                <button type="submit" name="view" class="btn btn-primary">View</button>
+              </div>
+            </div>
+        </form>
+        <?php
+        if (isset($_POST['view']) && isset($_POST['subject'])) {
+
+          $subject = $_POST['subject'];
+          $query = "SELECT * FROM assignment_new where subject='$subject'";
+          $data = mysqli_query($con, $query);
+          $total = mysqli_num_rows($data);
+          if ($total > 0) {
+        ?>
+            <table class="table-design" style="text-align:center">
+              <thead class="table-dark bg-primary">
+                <tr>
+                  <th scope="col">Serial No.</th>
+                  <th scope="col">Date</th>
+                  <th scope="col">Assignment</th>
+                </tr>
+              </thead>
+              <?php
+              $counter = 1;
+              while ($result = mysqli_fetch_array($data)) {
+              ?>
+                <tbody class="table-design">
+                  <tr>
+                    <th scope="row"><?php echo $counter++; ?></th>
+                    <td><?php echo $result['submit_date'] ?></td>
+                    <td><a href="<?php echo $result['link'] ?>">Click Here</a></td>
+
+                  </tr>
+                </tbody>
+
+          <?php
+              }
+            } else {?>
+              <div class="p-2"></div>
+              <div class="container d-flex justify-content-center">
+              <div class="d-flex justify-content-center alert alert-info" style="width:30%">
+              <?php echo "No Assignment Uploaded Yet";?>
+              </div>
+              </div>
+            <?php }
+          }
+          ?>
+
+            </table>
+
+      </div>
+      <!---container----->
+    </div>
+    <!---page content wrapper---->
+  </div>
+  <!---toggled----->
 </body>
+
 </html>
--- New/studyview.php
+++ New/studyview.php
@@ -19,22 +19,51 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  <script src="navigation.js"></script>
+  <link href="navigation.css" rel="stylesheet" id="naviagtion-css">
+  <link rel="stylesheet" href="cardstyles.css">
+  <link rel="stylesheet" href="tablestyle.css">
+  <style>
+    body {
+      background-color: #ededed;
+      background-repeat: no-repeat;
+      background-position: center;
+      background-size: contain;
+    }
+  </style>
 </head>
 <body>
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-  <ul class="navbar-nav">
-    <li class="nav-item active">
-      <a class="nav-link" href="#">Student Dashboard</a>
-    </li>
-  </ul>
+<nav class="navbar navbar-expand navbar-dark bg-primary fixed-top"> <a href="#menu-toggle" id="menu-toggle" class="navbar-brand"><span class="navbar-toggler-icon"></span></a> <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+    <div class="collapse navbar-collapse" id="navbarsExample02">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active"> <a class="nav-link" href="firstteacher.php">Dashboard</a> </li>
+      </ul>
+      <form class="form-inline my-2 my-md-0"> </form>
+    </div>
 </nav>
-<div class="container mt-5">
+<div id="wrapper" class="toggled">
+<div id="sidebar-wrapper">
+      <ul class="sidebar-nav">
+        <li class="sidebar-brand"> <a href="#"> <?php echo $_SESSION['username'] ?> </a> </li>
+        <li> <a href="firstteacher.php">Dashboard</a> </li>
+        <li> <a href="assignmentinside.php">Assignments</a> </li>
+        <li> <a href="noticeview.php">Notices</a> </li>
+        <li> <a href="studyupload.php">Study Material</a> </li>
+        <li> <a href="#">Contact</a> </li>
+      </ul>
+</div> <!-- /#sidebar-wrapper -->
+<div id="page-content-wrapper">
+<div class="container mt-3 custom-form-container " style="width:1000px; margin:0 auto;">
     <form action=" " method="post">
-        <h3>Study Materials</h3>
+        <h2 style="text-align:center">Study Materials</h2>
             <!-----<form>------>
-                <div class="form-group">
-                        <label for="subject">Select Subject</label>
-                            <select class="form-control" id="subjectControlSelect1" name="subject">           
+                <div class="p-2"></div>
+                <div class="form-group text-center">
+                <div class="container">
+                <div class="row d-flex justify-content-center">
+                        <label for="subject">Subject</label>
+                        <div class="p-2"></div>
+                            <select class="form-control w-25 col-xs-6" id="subjectControlSelect1" name="subject">           
                                 <?php
                                         $get_subject="select * from sem_subject where sem=$semester"; 	
                                         $run_subject=mysqli_query($con,$get_subject);
@@ -46,9 +75,11 @@
                                                 
                                 ?>            
                             </select>
-                            <div class="p-1"></div>
-                            <button type="submit" name="view" class="btn btn-primary">View</button>
-                </div>
+                            <div class="p-2"></div>
+                            <button type="submit" name="view" class="btn btn-primary  col-xs-6">View</button>
+                </div><!---row---->
+                </div><!---container---->
+                </div><!---form group text center----->
     </form>
     <?php
               if(isset($_POST['view']) && isset($_POST['subject'])){
@@ -59,8 +90,8 @@
 					$total=mysqli_num_rows($data);
                     if($total>0){
     ?>                
-                        <table class="table">
-                        <thead>
+                        <table class="table-design">
+                        <thead class="table-dark bg-primary">
                                 <tr>
                                         <th scope="col">Serial No.</th>
                                         <th scope="col">Topic Name</th>
@@ -72,7 +103,7 @@
                                 while($result =mysqli_fetch_array($data))
                                 {                        
 				        ?>
-                                    <tbody>
+                                    <tbody class="table-design">
                                     <tr>
                                         <th scope="row"><?php echo $counter++ ;?></th>
                                         <td><?php echo $result['name'] ?></td>
@@ -86,14 +117,21 @@
                     }
                      
                     else
-                    {
-                            echo "No Study Materials Uploaded Yet";
-                    }
+                    {?>
+                    <div class="p-2"></div>
+                    <div class="container d-flex justify-content-center">
+                    <div class="d-flex justify-content-center alert alert-info" style="width:30%">
+                            <?php echo "No Study Materials Uploaded Yet"; ?>
+                    </div>
+                    </div>
+                    <?php }
             }
 ?>
     
 </table>              
     
 </div>
+</div><!--page content wrapper ----->
+</div><!---toggled------->
 </body>
 </html>
