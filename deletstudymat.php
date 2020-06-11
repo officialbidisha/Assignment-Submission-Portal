fe12fdaa65be5cbd2cf12b7bd62310d62ead5770
Bidisha <officialbidisha1@gmail.com>
2020-06-11 23:48:39 +0530

    Delete Modified

--- deletstudymat.php
+++ deletstudymat.php
@@ -65,160 +65,158 @@ if (isset($_GET['sem'])) {
                     <h2>Delete Study Material</h2>
                 </center>
                 <div class="p-2"></div>
-                <form>
-                    <div class="form-group text-center">
-                        <div class="container">
-                            <div class="row d-flex justify-content-center">
-                                &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp;
-                                <label for="sem">Semester</label>
-                                <div class="p-2"></div>
-                                <input id="semsub" type="number" class="form-control w-25 col-xs-6" id="num" value="<?php echo $sem_sub; ?>" name="sem" aria-describedby="semester" placeholder="Enter Semester">
-                                <div class="p-2"></div>
-                                <button type="submit" class="btn btn-primary col-xs-6">Search</button>
+                <div class="border-decoration">
+                    <form>
+                        <div class="form-group text-center">
+                            <div class="container">
+                                <div class="row d-flex justify-content-center">
+
+                                    <label for="sem">Semester</label>
+                                    <div class="p-2"></div>
+                                    <input id="semsub" type="number" class="form-control w-25 col-xs-6" id="num" value="<?php echo $sem_sub; ?>" name="sem" aria-describedby="semester" placeholder="Enter Semester">
+                                    <div class="p-2"></div>
+                                    <button type="submit" class="btn btn-primary col-xs-6">Search</button>
+                                </div>
+                                <!--row----->
                             </div>
-                            <!--row----->
+                            <!--container------>
                         </div>
-                        <!--container------>
-                    </div>
-                    <!---form group----->
+                        <!---form group----->
+                    </form>
+                    <?php
+                    if (isset($_GET["sem"]) && $_GET['sem'] != "") {
+                        $get_subject = "select * from sem_subject where sem=$sem_sub AND teacher='$username'";
+                        $run_subject = mysqli_query($con, $get_subject);
 
+                        if (mysqli_num_rows($run_subject) > 0) {
+                    ?>
+                            <form action="" method="post" class="form-inline">
 
-                </form>
-                <?php
-                if (isset($_GET["sem"]) && $_GET['sem'] != "") {
-                    $get_subject = "select * from sem_subject where sem=$sem_sub AND teacher='$username'";
-                    $run_subject = mysqli_query($con, $get_subject);
+                                <input name="semester" value="<?php echo $_GET["sem"]; ?>" type="hidden">
+                                <div class="form-inline" style="margin:auto">
+                                    <div class="row d-flex justify-content-center">
+                                        <div class="text-center ">
+                                            <label for="exampleFormControlSelect1">Subject</label>
+                                        </div>
+                                        <div class="p-4"></div>
+                                        <div class="form-group col-xs-6 w-50">
 
-                    if (mysqli_num_rows($run_subject) > 0) {
-                ?>
-                        <form action="" method="post" class="form-inline">
-                            
-                            <input name="semester" value="<?php echo $_GET["sem"]; ?>" type="hidden">
-                            <div class="border-decoration">
-                            <div class="row d-flex justify-content-center">
-                                <div class="text-center ">
-                                    <label for="exampleFormControlSelect1">Subject</label>
-                                </div>
-                                <div class="p-4"></div>
-                                <div class="form-group col-xs-6 w-50">
+                                            <select class="form-control" name="subject">
+                                                <!---the subject is the variable where the selected item is stored-->
 
-                                    <select class="form-control" name="subject">
-                                        <!---the subject is the variable where the selected item is stored-->
+                                                <?php
+                                                while ($row_subject = mysqli_fetch_array($run_subject)) {
 
-                                        <?php
+                                                    $subject = $row_subject['subject'];
+                                                    echo "<option>$subject</option>";
+                                                }
+                                                ?>
 
-                                        //$sem_sub=$_GET["sem"];
+                                            </select>
 
-
-                                        while ($row_subject = mysqli_fetch_array($run_subject)) {
-
-                                            $subject = $row_subject['subject'];
-                                            echo "<option>$subject</option>";
-                                        }
-
-
-
-                                        ?>
-
-                                    </select>
-                                    <div class="mr-2"></div>
-                                    <div class="d-flex justify-content-center">
-                                    <button type="submit" name="upload" class="btn btn-primary">Select</button>
+                                        </div>
+                                        <div class="mr-4"></div>
+                                        <div class="d-flex justify-content-center">
+                                            <button type="submit" name="upload" class="btn btn-primary ">Select</button>
+                                        </div>
+                                        <div class='p-2'></div>
                                     </div>
+
                                 </div>
-                                <div class='p-2'></div>
-                            </div>
-                            
-                            </div><!---table decoration----->
-                            <div class="p-2"></div>
-                           
-            </div>
-            </form>
-            <div class="p-2"></div>
-    <?php
-                    } else { ?>
-                    <div class="p-2"></div>
-                    <div class="container d-flex justify-content-center">
+                                <!---table decoration----->
+                                <div class="p-2"></div>
+
+                
+                </form>
+                <div class="p-2"></div>
+            <?php
+                        } else { ?>
+                <div class="p-2"></div>
+                <div class="container d-flex justify-content-center">
                     <div class="d-flex justify-content-center alert alert-danger" style="width:30%">
                         <?php echo "Enter correct semester"; ?>
                     </div>
-                    </div>
-                    <?php }
-                } else {?> 
-                <div class="p-2"></div>
-                <div class="container d-flex justify-content-center">
-                <div class="d-flex justify-content-center alert alert-primary" style="width:30%">
-                   <?php  echo 'Search a semester first'; ?>
                 </div>
-                </div>
-                <?php }
-    ?>
-    <?php
-    if (isset($_POST['subject']) && isset($_POST['semester'])) {
-
-        $subject=$_POST['subject'];
-        $query = "SELECT * FROM study_material where subject='$subject'";
-        $data = mysqli_query($con, $query);
-        if(mysqli_num_rows($data)>0){?>
-        <form action="" method="post" class="form-inline">
-        <div class="border-decoration">
-        <div class="row d-flex justify-content-center">
-        <div class="text-center ">
-            <label for="exampleFormControlSelect1">Topic</label>
-        </div>
-        <div class="p-2"></div>
-        <div class="form-group col-xs-6 w-50">
-        <input type="hidden" name="semester" value="<?php echo $_POST['semester'] ?>">
-        <input type="hidden" name="subject" value="<?php echo $_POST['subject'] ?>">
-        <select class="form-control" name="name">
-        <?php
-         
-         while ($row_subject = mysqli_fetch_array($data)) {
-
-                                            $name = $row_subject['name'];
-                                            echo "<option>$name</option>";
-                                        }
-                                        ?>
-
-                                    </select>
-                                    <div class="mr-2"></div>
-                                    <div class="d-flex justify-content-center">
-                                    <button type="submit" name="upload" class="btn btn-primary">Delete</button>
-                                    </div>
-
-        </div>
-        </div>
-        </div>
-        </form>
-        <?php } else { ?>
+            <?php }
+                    } else { ?>
             <div class="p-2"></div>
             <div class="container d-flex justify-content-center">
-            <div class="d-flex justify-content-center alert alert-danger" style="width:30%">
-                <?php echo "No Study Materials";?>
-            </div>
+                <div class="d-flex justify-content-center alert alert-primary" style="width:30%">
+                    <?php echo 'Search a semester first'; ?>
+                </div>
             </div>
         <?php }
-    }
-    ?>
-    <?php
+        ?>
+        <?php
+        if (isset($_POST['subject']) && isset($_POST['semester'])) {
+
+            $subject = $_POST['subject'];
+            $query = "SELECT * FROM study_material where subject='$subject'";
+            $data = mysqli_query($con, $query);
+            if (mysqli_num_rows($data) > 0) { ?>
+                <form action="" method="post" class="form-inline">
+                    <div class="form-inline" style="margin:auto">
+                        <div class="row d-flex justify-content-center">
+                            <div class="text-center ">
+                                <label for="exampleFormControlSelect1">Topic</label>
+                            </div>
+                            <div class="p-2"></div>
+                            <div class="form-group col-xs-6 w-50">
+                                <input type="hidden" name="semester" value="<?php echo $_POST['semester'] ?>">
+                                <input type="hidden" name="subject" value="<?php echo $_POST['subject'] ?>">
+                                <select class="form-control" name="name">
+                                    <?php
+                                    while ($row_subject = mysqli_fetch_array($data)) {
+                                        $name = $row_subject['name'];
+                                        echo "<option>$name</option>";
+                                    } ?>
+                                </select>
+                                
+                                
+                            </div>
+                            <div class="mr-2"></div>
+                            <div class="d-flex col-xs-6 justify-content-center">
+                                    <button type="submit" name="upload" class="btn btn-primary">Delete</button>
+                            </div>
+                        </div><!---row end--->
+                    </div> 
+                </form>
+                </div>
+            <?php } else { ?>
+                <div class="p-2"></div>
+                <div class="container d-flex justify-content-center">
+                    <div class="d-flex justify-content-center alert alert-danger" style="width:30%">
+                        <?php echo "No Study Materials"; ?>
+                    </div>
+                </div>
+        <?php }
+        }
+        ?>
+        <?php
         if (isset($_POST['subject']) && isset($_POST['semester']) && isset($_POST['name'])) {
-            $subject=$_POST['subject'];
-            $sem_sub=$_POST['semester'];
-            $name=$_POST['name'];
-            $query="DELETE FROM study_material where subject='$subject' AND sem='$sem_sub' AND name='$name'";
-            $query1=mysqli_query($con,$query);
-            if($query1){
-                            echo "Study Material deleted";
-            }
-            else{
-                    echo "No";
-            }
-        }
-        else{
+            $subject = $_POST['subject'];
+            $sem_sub = $_POST['semester'];
+            $name = $_POST['name'];
+            $query = "DELETE FROM study_material where subject='$subject' AND sem='$sem_sub' AND name='$name'";
+            $query1 = mysqli_query($con, $query);
+            if ($query1) { ?>
+                <div class="p-2"></div>
+                <div class="container d-flex justify-content-center">
+                    <div class="d-flex justify-content-center alert alert-success" style="width:30%">
 
+                        <?php echo "Study Material deleted"; ?>
+                    </div>
+                </div>
+
+        <?php } else {
+                echo "No";
+            }
+        } else {
         }
 
-    ?>
+        ?>
+          
+        </div>
         </div>
         <!--container mt-5----->
     </div>
