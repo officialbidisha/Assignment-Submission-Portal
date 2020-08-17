c1e8138b3f4e32bbacae420c0ebdef3f1ec1e958
DoraSaha1 <52519828+DoraSaha1@users.noreply.github.com>
2020-08-17 21:31:57 +0530

    Update deletenotice.php

--- deletenotice.php
+++ deletenotice.php
@@ -28,6 +28,7 @@ $semester = !empty($_GET['sem']) ? $_GET['sem'] : '';
     }
   </style>
 </head>
+
 <body>
   <nav class="navbar navbar-expand navbar-dark bg-primary fixed-top"> <a href="#menu-toggle" id="menu-toggle" class="navbar-brand"><span class="navbar-toggler-icon"></span></a> <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
     <div class="collapse navbar-collapse" id="navbarsExample02">
